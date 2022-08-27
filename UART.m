9470f6064470b59951b6ebf4dc9171b7eebe6e65
MadejSzymon <110915050+MadejSzymon@users.noreply.github.com>
2022-08-27 19:58:00 +0200

    Update UART.m

--- UART.m
+++ UART.m
@@ -1,20 +1,14 @@
 clear 
-s = serialport('COM5',115200,'Parity','none','Timeout',120);
-data = read(s,4096,"int16");
+COM = 'COM5';
+baud_rate = 9600;
+parity = 'none';
+timeout = 120;
+nbr_data = 5000;
+data_type = "uint8";
+
+s = serialport(COM,baud_rate,'Parity',parity,'Timeout',timeout);
+data = read(s,nbr_data,data_type);
 fopen(s);
 
-%%
-Fs = 25000;            % Sampling frequency                    
-T = 1/Fs;             % Sampling period       
-L = 10000;             % Length of signal
-t = (0:L-1)*T;        % Time vector
-X = data;
-Y = fft(X);
-P2 = abs(Y/L);
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-f = Fs*(0:(L/2))/L;
-plot(f,P1) 
-title('Single-Sided Amplitude Spectrum of X(t)')
-xlabel('f (Hz)')
-ylabel('|P1(f)|')
+plot(data)
+
